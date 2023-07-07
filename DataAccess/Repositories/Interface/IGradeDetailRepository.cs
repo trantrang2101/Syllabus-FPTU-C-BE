2fd8f7e596766b555f703cf2718d7f0ae0d36511
Lê Thành Đạt <datdatle2002@gmail.com>
2023-07-07 20:37:25 +0700

    adding more repository

---
+++ DataAccess/Repositories/Interface/IGradeDetailRepository.cs
@@ -0,0 +1,14 @@
+﻿using BusinessObject.Models;
+using DataAccess.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Repositories.Interface
+{
+    public interface IGradeDetailRepository : IBaseRepository<GradeDetail, GradeDetailDTO>
+    {
+    }
+}
