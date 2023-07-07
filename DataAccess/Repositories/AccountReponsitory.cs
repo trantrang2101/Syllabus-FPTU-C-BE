7122248c9093e283cf3e1cbc40c3112cccd0826b
Lê Thành Đạt <datdatle2002@gmail.com>
2023-07-07 10:22:04 +0700

    finishing accountrepository

--- DataAccess/Repositories/AccountReponsitory.cs
+++ DataAccess/Repositories/AccountReponsitory.cs
@@ -21,8 +21,8 @@ namespace DataAccess.Repositories
             {
                 throw new Exception("Trường Email là trường bắt buộc");
             }
-            Account acc = table.Where(x => x.Email.ToLower() == gmail.ToLower())
-                .Include(x => x.AccountRoles).ThenInclude(x=>x.Role).FirstOrDefault();
+            Account? acc = table.Where(x => x.Email.ToLower() == gmail.ToLower())
+                .Include(x => x.AccountRoles).ThenInclude(x => x.Role).FirstOrDefault();
             if (acc == null)
             {
                 throw new Exception("Không tìm thấy tài khoản");
@@ -34,13 +34,44 @@ namespace DataAccess.Repositories
 
         public override List<AccountDTO> GetAll()
         {
-            List<Account> products = _context.Accounts.Include(x=>x.AccountRoles).ThenInclude(x=>x.Role).ToList();
+            List<Account> products = _context.Accounts.Include(x => x.AccountRoles).ThenInclude(x => x.Role).ToList();
             List<AccountDTO> dto = new List<AccountDTO>() { };
             if (products != null && products.Count() > 0)
             {
                 return _mapper.Map<List<AccountDTO>>(products);
             }
             return dto;
+        }
+
+        public override AccountDTO Get(long id)
+        {
+            var account = _context.Accounts.FirstOrDefault(x => x.Id == id);
+            var dto = new AccountDTO();
+            if (account != null)
+            {
+                return _mapper.Map<AccountDTO>(account);
+            }
+            return dto;
+        }
+
+        public override AccountDTO Update(AccountDTO dto)
+        {
+            var account = _mapper.Map<AccountDTO>(dto);
+            var entityChanged = _context.Update(account).Entity;
+            _context.SaveChanges();
+            return entityChanged;
+        }
+
+        public override bool Delete(long id)
+        {
+            var account = _context.Accounts.FirstOrDefault(x => x.Id == id);
+            if (account != null)
+            {
+                _context.Accounts.Remove(account);
+                var indexChanged = _context.SaveChanges();
+                return (indexChanged > 0);
+            }
+            return false;
         }
     }
 }
